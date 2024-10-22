3fc24e0e831d0fea753454edd7248bc1af6e81ce
Nathaniel Faborada <143235940+nathanielfaborada@users.noreply.github.com>
2024-10-22 10:34:06 +0800

    Update login.php

--- e-commerce/login.php
+++ e-commerce/login.php
@@ -6,13 +6,17 @@ error_reporting(E_ALL);
 
 include('connect.php'); 
 
-
 if (isset($_POST['signIn'])) {
     $username = trim($_POST['username']);
     $password = ($_POST['password']); 
 
-    $sql = "SELECT password FROM customer_credentials WHERE username = '$username'";
-    $result = $conn->query($sql);
+
+    $sql = "SELECT password FROM customer_credentials WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $hashed_password = $row['password'];
@@ -26,9 +30,30 @@ if (isset($_POST['signIn'])) {
             echo 'Incorrect username or password';
         }
     } else {
-        echo 'User not found';
+        $sql = "SELECT password FROM admin WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $hashed_password = $row['password'];
+
+            if (password_verify($password, $hashed_password)) {
+                session_start();
+                $_SESSION["username"] = $username;
+                header("Location: adminHome.php");
+                exit();
+            } else {
+                echo 'Incorrect username or password';
+            }
+        } else {
+            echo 'User not found';
+        }
     }
 
+    $stmt->close();
     $conn->close();
 }
 ?>
