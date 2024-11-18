6b0c222817d5db4115b94175e611a13dde2b4f2b
Diether Pano <143235932+DietherPano@users.noreply.github.com>
2024-11-18 10:14:48 +0800

    N!66@5

--- e-commerce/user_profile.php
+++ e-commerce/user_profile.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/dashboard.css">
+  <title>Profile Page</title>
 </head>
 <body>
-    <div class="side-bar">
-        <H1>Under Construction</H1>
-    </div>
+  <div class="container">
+    <aside class="sidebar">
+        <img src="./images/profile_svg/home-icon-silhouette-svgrepo-com.svg" alt="home icon" class="home-icon">
+        <img src="./images/profile_svg/car-svgrepo-com.svg" alt="cart icon" class="cart-icon">
+        <img src="./images/profile_svg/log-out-svgrepo-com.svg" alt="logout icon" class="logout-icon">
+    </aside>
+    <main class="profile">
+      <div class="profile-header">
+        <div class="profile-picture">
+          <img src="profile-picture.jpg" alt="Profile Picture">
+        </div>
+        <h2 class="profile-name">Johnny Weak</h2>
+      </div>
+      <div class="profile-info">
+        <p><strong>Email:</strong> whatdaheil@gmail.com</p>
+        <p><strong>Address:</strong> ahahahahaha st. bwahahahaha</p>
+      </div>
+    </main>
+  </div>
 </body>
 </html>
